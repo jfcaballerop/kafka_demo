a2d46b4d3e73070b8298631d1d8edd599c27fa1b
jfcaballerop <jfcaballerop@gmail.com>
2023-10-16 20:33:14 +0200

    add basic listener

---
+++ demo-producer/src/main/java/com/mrknight/pocs/demoproducer/services/ReceiveMsgSvc.java
@@ -0,0 +1,23 @@
+package com.mrknight.pocs.demoproducer.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReceiveMsgSvc {
+
+  @Value(value = "${kafka.topic.name}")
+  private String topic;
+
+  @Value(value = "${spring.kafka.consumer.group-id}")
+  private String groupId;
+
+  @KafkaListener(topics = "${kafka.topic.name}")
+  public void listener(@Payload String message) {
+    System.out
+        .println("Received Message from Topic [" + topic + "] in group: " + groupId + " msg: [ " + message + " ]");
+  }
+
+}
